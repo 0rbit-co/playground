897adafb4cd2f9a7f21eb376039969e9b459624a
L-Pircy <taveesha.agarwal@gmail.com>
2024-05-23 01:09:32 +0530

    codeblock re

--- app/_components/BetterIDEa.tsx
+++ app/_components/BetterIDEa.tsx
@@ -6,10 +6,23 @@ import { CodeCell, runCell } from "@betteridea/codecell";
 const BetterIDEa = () => {
   const curCode = useCodeStore((state) => state.curCode);
   const [a, setA] = useState("//Enter Code Here...");
+  const [render, setRender] = useState(true);
+
   useEffect(() => {
     console.log(encodeURI(curCode));
     setA(curCode);
   }, [curCode]);
+
+  useEffect(() => {
+    setRender(false);
+    setTimeout(() => {
+      setRender(true);
+    }, 1000);
+  }, [a]);
+
+  if (!render) {
+    return null;
+  }
 
   return <CodeCell height="100%" cellId="1" appName="Sandbox" code={a} />;
 };

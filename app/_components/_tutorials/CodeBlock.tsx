f71a128a3fa861ec59421700baecc5c6c2f3730b
L-Pircy <taveesha.agarwal@gmail.com>
2024-05-23 02:13:46 +0530

    scroll

--- app/_components/_tutorials/CodeBlock.tsx
+++ app/_components/_tutorials/CodeBlock.tsx
@@ -3,7 +3,7 @@ import PrismLoader from "../PrismLoader";
 
 const CodeBlock = ({ step }: any) => {
   return (
-    <pre className="language-lua w-full !bg-[#61625F]">
+    <pre className="language-lua w-full !bg-[#61625F] codeblock">
       <code className="language-lua">{step}</code>
       <PrismLoader />
     </pre>

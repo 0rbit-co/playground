288d71c816b749a8d49424877eee466dd8050e9e
L-Pircy <taveesha.agarwal@gmail.com>
2024-05-26 10:18:57 +0530

    copy btn

--- app/_components/BetterIDEa.tsx
+++ app/_components/BetterIDEa.tsx
@@ -2,11 +2,17 @@
 import React, { useEffect, useState } from "react";
 import { useCodeStore } from "../_store/store";
 import { CodeCell, runCell, setCellCode } from "@betteridea/codecell";
-import { brandDarkText, brandSecondaryBg } from "../_utils/colors";
+import {
+  brandDarkText,
+  brandLightText,
+  brandSecondaryBg,
+} from "../_utils/colors";
+import Image from "next/image";
 
 const BetterIDEa = () => {
   const curCode = useCodeStore((state) => state.curCode);
   const [curPid, setCurPid] = useState("");
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     console.log(encodeURI(curCode));
@@ -19,6 +25,22 @@ const BetterIDEa = () => {
     setCurPid(pid);
     console.log(pid);
   };
+
+  const handleCopy = () => {
+    if (curPid) {
+      navigator.clipboard
+        .writeText(curPid)
+        .then(() => {
+          setCopied(true);
+          console.log("Copied to clipboard:", curPid);
+        })
+        .catch((err) => {
+          setCopied(false);
+          console.error("Failed to copy:", err);
+        });
+    }
+  };
+
   return (
     <>
       <CodeCell
@@ -27,11 +49,30 @@ const BetterIDEa = () => {
         appName="Sandbox"
         onAOProcess={onAOProcess}
       />
-      <h3
-        className={`font-semibold text-[12px] ${brandDarkText} my-[6px] ${brandSecondaryBg} px-[12px] py-[3px] rounded-sm`}
+      <div
+        className={`flex flex-row items-center justify-start gap-3 font-semibold text-[12px] ${brandLightText} my-[6px] ${brandSecondaryBg} px-[12px] py-[3px] rounded-sm`}
       >
-        {curPid ? `Process ID:${curPid}` : "Please Connect"}
-      </h3>
+        <h3> {curPid ? `Process ID:${curPid}  ` : "Please Connect"}</h3>
+        {curPid && copied ? (
+          <Image
+            alt="copy"
+            src="/clip1.png"
+            height={18}
+            width={18}
+            onClick={handleCopy}
+            className="hover:cursor-pointer w-[18px] h-[18px] hover:bg-[#7f512a] rounded-md"
+          />
+        ) : (
+          <Image
+            alt="copy"
+            src="/clip0.png"
+            height={18}
+            width={18}
+            onClick={handleCopy}
+            className="hover:cursor-pointer w-[18px] h-[18px] hover:bg-[#7f512a] rounded-md"
+          />
+        )}
+      </div>
     </>
   );
 };

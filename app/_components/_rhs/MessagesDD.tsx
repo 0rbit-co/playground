45479e348664151391536c2e2a1e698d3f8fd0f5
L-Pircy <taveesha.agarwal@gmail.com>
2024-06-01 13:03:26 +0530

    pid poweredby responsive fixed, non terminal displat working

--- app/_components/_rhs/MessagesDD.tsx
+++ app/_components/_rhs/MessagesDD.tsx
@@ -1,17 +1,27 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { brandLightText } from "../../_utils/colors";
 import { getInbox } from "@betteridea/codecell";
 import Image from "next/image";
-import { useMessagesArrayStore } from "../../_store/store";
+import { useMessagesArrayStore, usePidStore } from "../../_store/store";
 
 const MessagesDD = () => {
   //   const [mesArr, setMesArr] = useState<any[]>([]);
   const [mesToggle, setMesToggle] = useState(false);
   const messArr = useMessagesArrayStore((state) => state.messArr);
+  const curPid = usePidStore((state) => state.curPid);
+
+  useEffect(() => {
+    {
+      curPid &&
+        setTimeout(() => {
+          getInbox("1", true);
+        }, 1000);
+    }
+  }, [messArr]);
 
   const stripAnsiCodes = (str: string) => {
-    str.replace(
+    return str?.replace(
       /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
       ""
     );
@@ -22,16 +32,18 @@ const MessagesDD = () => {
         text-[12px] ${brandLightText} mt-[-12px]
         `}
     >
-      <div
+      <button
+        // disabled={!curPid}
         onClick={() => {
           getInbox("1", true);
           setMesToggle(!mesToggle);
         }}
-        className="flex flex-row justify-start gap-3 items-center"
+        className={`flex flex-row justify-start gap-3 items-center
+        hover:tracking-widest transition-[all_0.3s] `}
       >
-        <button className="uppercase text-[15px] text-[#98e870]">
+        <h3 className="uppercase text-[15px] text-[#98e870]">
           {mesToggle ? "Messages" : "Check Inbox"}
-        </button>
+        </h3>
         <Image
           src="/ddMes.png"
           width={15}
@@ -39,15 +51,17 @@ const MessagesDD = () => {
           alt="dd"
           className={`${mesToggle ? "rotate-180" : "rotate-0"}`}
         />
-      </div>
+      </button>
       <div className={`max-h-[60px] ${mesToggle ? "overflow-y-scroll" : ""}`}>
         {mesToggle &&
           messArr?.map((item, id) => {
             stripAnsiCodes(item.Data);
             return (
               <div className=" tracking-[1.5px]">
-                {`Inbox[${id + 1}].Data: `}
-                {item.Data ? item.Data : "No Data"}
+                <span className="text-[#98e870]">{`Inbox[${
+                  id + 1
+                }].Data: `}</span>
+                {item.Data ? stripAnsiCodes(item.Data) : "No Data"}
               </div>
             );
           })}
